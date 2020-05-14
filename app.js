8a638be975e79f34206779623d28488213fd0013
ryantoddgarza <ryantoddgarza@gmail.com>
2020-05-13 17:06:31 -0700

    user input

--- app.js
+++ app.js
@@ -15,13 +15,52 @@ setTimeout(function typeMessage() {
     function task(i) {
       setTimeout (function() {
         messageOutput.push(messageContent[i]);
-        console.log(messageOutput);
         domMessage.textContent = messageOutput.join('');
       }, 200 * i);
     }
   }
+
+  document.addEventListener('keydown', function(event) {
+    let key = event.key;
+
+    switch (key) {
+      case 'Backspace':
+        messageOutput.pop();
+        break;
+
+      case 'Shift':
+      case 'Meta':
+      case 'Alt':
+      case 'Control':
+      case 'Escape':
+      case 'Tab':
+      case 'CapsLock':
+      case 'Enter':
+        // Ignore modifier keys
+        break;
+
+      default:
+          messageOutput.push(key);
+        break;
+    }
+    domMessage.textContent = messageOutput.join('');
+  })
+
 }, 1250);
 
-(setTimeout(function showSocial() {
+setTimeout(function showSocial() {
   document.querySelector('.social').style.opacity = '1';
-}, 4000))();
+}, 4000);
+
+setTimeout(function showKeyboard() {
+  const icon = document.querySelector('.keyboard-container');
+  icon.style.opacity = '1';
+}, 8000);
+
+function hideKeyboardIcon() {
+  document.querySelector('.keyboard-container').classList.add('keyboard-open');
+}
+
+document.querySelector('.keyboard-container').addEventListener('click', function() {
+  hideKeyboardIcon();
+})
